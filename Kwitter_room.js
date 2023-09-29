77d2a6054af479ed8c4a2e7d4e4c96c07f803342
JHANVI GOWDA <124856292+forehandpower@users.noreply.github.com>
2023-09-28 19:49:14 -0700

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAIkDU72ryLpEJT85kp9pHxAPJR8N_GJ5M",
+    authDomain: "foodie-fed5a.firebaseapp.com",
+    databaseURL: "https://foodie-fed5a-default-rtdb.firebaseio.com",
+    projectId: "foodie-fed5a",
+    storageBucket: "foodie-fed5a.appspot.com",
+    messagingSenderId: "134042796987",
+    appId: "1:134042796987:web:b07e390da3d9e1fe80fa9e"
+  };
+  
+firebase.initializeApp(firebaseConfig);
